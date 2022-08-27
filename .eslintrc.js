57451de19dcbf0fa573dc37d25935b27f4af415e
Leoander Neves <leoanderneves8@outlook.com>
2022-08-27 16:09:33 -0300

    create .eslintrc.js

---
+++ .eslintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+  'env': {
+    'es2021': true,
+    'node': true,
+  },
+  'extends': 'eslint:recommended',
+  'overrides': [
+  ],
+  'parserOptions': {
+    'ecmaVersion': 'latest',
+    'sourceType': 'module',
+  },
+  'rules': {
+    // enforce camelcase naming convention
+    'camelcase': 'off',
+    // enforce the consistent use of either backticks, double, or single quotes
+    'quotes': ['error', 'single'],
+    // we use 2 spaces to indent our code
+    'indent': ['error', 2],
+    // we want to avoid extraneous spaces
+    'no-multi-spaces': ['error'],
+    // disallow specified warning terms in comments
+    'no-warning-comments': 'warn',
+    'comma-dangle': ['error', {
+      arrays: 'always-multiline',
+      objects: 'always-multiline',
+      imports: 'always-multiline',
+      exports: 'always-multiline',
+      functions: 'always-multiline',
+    }],
+  },
+}
